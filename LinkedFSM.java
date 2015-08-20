139d59de5a4af92ca9572edc9d2eee5f05dff369
Seth Ebner <murgrehk@gmail.com>
2015-08-20 12:34:20 -0400

    Fix spacing issues

--- LinkedFSM.java
+++ LinkedFSM.java
@@ -4,112 +4,112 @@ import java.util.ArrayList;
 
 public class LinkedFSM {
 
-	private State start;
-	private ArrayList<State> allStates;
-	private ArrayList<State> inhabitedStates;
-	private State deadState;
-	private ArrayList<Character> terminals;
-	//TODO: add in terminal alphabet and a check to make sure the input contains symbols only from that alphabet
-	//TODO: add in transition table and method to automatically create transitions from it
+    private State start;
+    private ArrayList<State> allStates;
+    private ArrayList<State> inhabitedStates;
+    private State deadState;
+    private ArrayList<Character> terminals;
+    //TODO: add in terminal alphabet and a check to make sure the input contains symbols only from that alphabet
+    //TODO: add in transition table and method to automatically create transitions from it
 
-	public LinkedFSM(boolean startAccepting, ArrayList<Character> terminals)
-	{
-		this.start = new State("START", startAccepting);
-		this.allStates = new ArrayList<State>(); //list of all the states in the FSM
-		this.allStates.add(this.start);
-		this.inhabitedStates = new ArrayList<State>(); //list of all the states in the FSM that are currently occupied
-		this.inhabitedStates.add(this.start);
-		this.terminals = terminals;
-		
-		this.deadState = new State("DEAD", false);
-		for (char c : terminals)
-		{
-			connect(deadState, deadState, c);
-		}
-	}
+    public LinkedFSM(boolean startAccepting, ArrayList<Character> terminals)
+    {
+        this.start = new State("START", startAccepting);
+        this.allStates = new ArrayList<State>(); //list of all the states in the FSM
+        this.allStates.add(this.start);
+        this.inhabitedStates = new ArrayList<State>(); //list of all the states in the FSM that are currently occupied
+        this.inhabitedStates.add(this.start);
+        this.terminals = terminals;
 
-	public State getStart() { return this.start; }
-	public ArrayList<State> getAllStates() { return this.allStates; }
-	public ArrayList<State> getInhabitedStates() { return this.inhabitedStates; }
+        this.deadState = new State("DEAD", false);
+        for (char c : terminals)
+        {
+            connect(deadState, deadState, c);
+        }
+    }
 
-	/**
-	 * Determines if there are no states in the FSM
-	 */
-	public boolean isEmpty()
-	{
-		return this.allStates.isEmpty();
-	}
+    public State getStart() { return this.start; }
+    public ArrayList<State> getAllStates() { return this.allStates; }
+    public ArrayList<State> getInhabitedStates() { return this.inhabitedStates; }
 
-	/**
-	 * Create a new state to use in the FSM
-	 */
-	public State createState(String id, boolean accepting)
-	{
-		State state = new State(id, accepting);
-		this.allStates.add(state);
-		return state;
-	}
+    /**
+     * Determines if there are no states in the FSM
+     */
+    public boolean isEmpty()
+    {
+        return this.allStates.isEmpty();
+    }
 
-	/**
-	 * Connects State A to State B with a transition that consumes character 'c'
-	 */
-	public boolean connect(State a, State b, char c)
-	{		
-		return (this.terminals.contains(c) ? a.setTransition(b, c) : false);
-	}
+    /**
+     * Create a new state to use in the FSM
+     */
+    public State createState(String id, boolean accepting)
+    {
+        State state = new State(id, accepting);
+        this.allStates.add(state);
+        return state;
+    }
 
-	/**
-	 * Given a list of states we are in, determines where we could be after consuming character 'c'
-	 */
-	public void processCharacter(char c)
-	{
-		if (!this.terminals.contains(c) && !this.inhabitedStates.contains(deadState))
-		{
-			this.inhabitedStates.clear();
-			this.inhabitedStates.add(deadState);
-		}
-		
-		ArrayList<State> destinations = new ArrayList<State>();
-		for(State state : this.inhabitedStates)
-		{
-			if(state.getTransitions().containsKey(c)) //there exists a transition to some state on character 'c'
-			{
-				for(State d : state.getNext(c))
-				{
-					if(!destinations.contains(d))
-					{
-						destinations.add(d);
-					}
-				}
-			}
-			if(state.getTransitions().containsKey('\0'))
-			{
-				for(State d : state.getNext('\0'))
-				{
-					if(!destinations.contains((d)))
-					{
-						destinations.add(d);
-					}
-				}
-			}
-		}
+    /**
+     * Connects State A to State B with a transition that consumes character 'c'
+     */
+    public boolean connect(State a, State b, char c)
+    {		
+        return (this.terminals.contains(c) ? a.setTransition(b, c) : false);
+    }
 
-		this.inhabitedStates = destinations;
-	}
+    /**
+     * Given a list of states we are in, determines where we could be after consuming character 'c'
+     */
+    public void processCharacter(char c)
+    {
+        if (!this.terminals.contains(c) && !this.inhabitedStates.contains(deadState))
+        {
+            this.inhabitedStates.clear();
+            this.inhabitedStates.add(deadState);
+        }
 
-	/**
-	 * Given a list of states we are in, determines if the amount of the string processed so far is accepted
-	 */
-	public boolean inAcceptingState()
-	{
-		for(State state : this.inhabitedStates)
-		{
-			if(state.isAccepting())
-			{
-				return true;
-			}
-		}
-		return false;
-	}
+        ArrayList<State> destinations = new ArrayList<State>();
+        for(State state : this.inhabitedStates)
+        {
+            if(state.getTransitions().containsKey(c)) //there exists a transition to some state on character 'c'
+            {
+                for(State d : state.getNext(c))
+                {
+                    if(!destinations.contains(d))
+                    {
+                        destinations.add(d);
+                    }
+                }
+            }
+            if(state.getTransitions().containsKey('\0'))
+            {
+                for(State d : state.getNext('\0'))
+                {
+                    if(!destinations.contains((d)))
+                    {
+                        destinations.add(d);
+                    }
+                }
+            }
+        }
+
+        this.inhabitedStates = destinations;
+    }
+
+    /**
+     * Given a list of states we are in, determines if the amount of the string processed so far is accepted
+     */
+    public boolean inAcceptingState()
+    {
+        for(State state : this.inhabitedStates)
+        {
+            if(state.isAccepting())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
--- Main.java
+++ Main.java
@@ -4,45 +4,45 @@ import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String args[])
-	{
-		ArrayList<Character> terminals = new ArrayList<Character>();
-		terminals.add('a');
-		terminals.add('b');
-		terminals.add('\0');
-		LinkedFSM fsm = new LinkedFSM(false, terminals);
-		
-		String inputString = "aaa";
-		char[] input = inputString.toCharArray(); //represent String an as array of characters to feed into the FSM one at a time
-		
-		//Create the states in the FSM
-		State q1 = fsm.createState("q1", true);
+    public static void main(String args[])
+    {
+        ArrayList<Character> terminals = new ArrayList<Character>();
+        terminals.add('a');
+        terminals.add('b');
+        terminals.add('\0');
+        LinkedFSM fsm = new LinkedFSM(false, terminals);
 
-		// Define the transition function
-		// fsm.connect(A, B, 'x') creates a transition from State A to State B by consuming the character 'x'
-		fsm.connect(fsm.getStart(), fsm.getStart(), 'a');
-		fsm.connect(fsm.getStart(), q1, 'b');
-		fsm.connect(fsm.getStart(), q1, '\0');
-		fsm.connect(q1, q1, 'a');
-		fsm.connect(q1, q1, 'b');
-		
-		for(char c : input)
-		{
-			System.out.print("Processing '" + c + "'. ");
-			fsm.processCharacter(c);
-			System.out.print("Now in state(s): ");
+        String inputString = "aaa";
+        char[] input = inputString.toCharArray(); //represent String an as array of characters to feed into the FSM one at a time
 
-			for(State s : fsm.getInhabitedStates())
-			{
-				s.printNode();
-			}
-			
-			System.out.print("\t");
-			System.out.println(fsm.inAcceptingState() ? "We're accepting at this point." : "We're not accepting at this point.");
-		}
-		
-		System.out.println(fsm.inAcceptingState() ? "INPUT ACCEPTED" : "INPUT REJECTED");
-		
-	}
-	
+        //Create the states in the FSM
+        State q1 = fsm.createState("q1", true);
+
+        // Define the transition function
+        // fsm.connect(A, B, 'x') creates a transition from State A to State B by consuming the character 'x'
+        fsm.connect(fsm.getStart(), fsm.getStart(), 'a');
+        fsm.connect(fsm.getStart(), q1, 'b');
+        fsm.connect(fsm.getStart(), q1, '\0');
+        fsm.connect(q1, q1, 'a');
+        fsm.connect(q1, q1, 'b');
+
+        for(char c : input)
+        {
+            System.out.print("Processing '" + c + "'. ");
+            fsm.processCharacter(c);
+            System.out.print("Now in state(s): ");
+
+            for(State s : fsm.getInhabitedStates())
+            {
+                s.printNode();
+            }
+
+            System.out.print("\t");
+            System.out.println(fsm.inAcceptingState() ? "We're accepting at this point." : "We're not accepting at this point.");
+        }
+
+        System.out.println(fsm.inAcceptingState() ? "INPUT ACCEPTED" : "INPUT REJECTED");
+
+    }
+
 }
